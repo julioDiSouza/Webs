a1b646a482808cb6c06bcf7b69b1fbbeabc3ee41
Julio Patricio de Souza Neto <julio.neto@onlineapp.com.br>
2020-02-04 15:37:07 -0300

    teste comentário

--- Webs.Web/Startup.cs
+++ Webs.Web/Startup.cs
@@ -63,7 +63,7 @@ namespace Webs.Web
 
                 if (env.IsDevelopment())
                 {
-                    //spa.UseAngularCliServer(npmScript: "start");
+                    /*spa.UseAngularCliServer(npmScript: "start");*/
                     spa.UseProxyToSpaDevelopmentServer("http://localhost:4200/");
                 }
             });
