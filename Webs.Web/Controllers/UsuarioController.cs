5d6ed13f80b4f22b7129471bb05c70f98b1447c3
Julio Patricio de Souza Neto <julio.neto@onlineapp.com.br>
2020-03-03 15:40:33 -0300

    Iniciando os Serviços

---
+++ Webs.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Webs.Domain.Entities;
+
+namespace Webs.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        [HttpPost("VerificarUsuario")]
+        public IActionResult VerificarUsuario([FromBody] Usuario usuario)
+        {
+			try
+			{
+                if (usuario.Email == "julio@teste" && usuario.Senha == "123")
+                    return Ok(usuario);
+
+                return BadRequest("Usuário ou senha inválido");
+            }
+			catch (Exception ex)
+			{
+                return BadRequest(ex.ToString());
+			}
+        }
+
+    }
+}
