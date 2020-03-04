a5cc05b3c772c31238768cfb3bca55b93b6271f9
Julio Patricio de Souza Neto <julio.neto@onlineapp.com.br>
2020-03-04 14:33:48 -0300

    Inicio modulo usuario e produtos

---
+++ Webs.Web/ClientApp/src/app/usuario/cadastro/cadastro.component.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CadastroUsuarioComponent = /** @class */ (function () {
+    function CadastroUsuarioComponent() {
+    }
+    return CadastroUsuarioComponent;
+}());
+exports.CadastroUsuarioComponent = CadastroUsuarioComponent;
+//# sourceMappingURL=cadastro.component.js.map
